2473f76b91b4f996a4264f34675a4b092533eb57
letie <letie@DESKTOP-7CNCDV2>
2022-09-30 14:56:23 +0700

    Add project files.

---
+++ api/Data/IssueDbContext.cs
@@ -0,0 +1,15 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class IssueDbContext: DbContext
+    {
+        public IssueDbContext(DbContextOptions<IssueDbContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
